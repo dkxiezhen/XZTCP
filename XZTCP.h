286475c3891bc5a256cff7c273e874ef0e6d42f5
fengdsd <dkxiezhen@126.com>
2016-07-15 09:18:03 +0800

    7.15
    修改block回调

--- XZTCP.h
+++ XZTCP.h
@@ -8,113 +8,25 @@
 
 #import <Foundation/Foundation.h>
 
-typedef enum : NSUInteger {
-    kGetDeviceStatusNetwork = 200,
-    kFindDeviceLocation,
-    kDeviceReconnect,
-    kGetDeviceDisplayData,
-    kGetHouseXFCtrlStatus,
-    kGetXFCtrlMode,
-    kSetXFCtrlAutoModeAvgParam,
-    kGetXFCtrlAutoModeAvgParam,
-    kSetXFCtrlAutoModeSensorParam,
-    kGetXFCtrlAutoModeSensorParam,
-    kSetXFCtrlManualModeParam,
-    kGetXFCtrlManualModeParam,
-    kSetXFCtrlTimerProfile,
-    kGetXFCtrlTimerProfile,
-    kSetXFCtrlTimerModeEnable,
-    kGetXFCtrlTimerModeEnable,
-    kGetCtrlBoxDisplay,
-    kDistributeNodeList,
-} APIKay;
-
 /**
  *  发送数据候回调函数
  */
 typedef void(^HttpSendBlock)(NSData * data);
-typedef void(^Connect)(BOOL isConnect);
+typedef void(^SendFailBlock)(void);
+typedef BOOL(^Connect)(BOOL isConnect);
 @class GCDAsyncSocket;
 @interface XZTCP : NSObject{
-
+    
     
 }
 
 @property (nonatomic, strong) GCDAsyncSocket * socket;
 @property (nonatomic, copy) Connect tcpConnect;
 /**
- *  批量获取设备网络状态
+ *  发送数据
  */
-@property (nonatomic, copy) HttpSendBlock GetDeviceStatusNetwork;
-/**
- *  寻找设备
- */
-@property (nonatomic, copy) HttpSendBlock FindDeviceLocation;
-/**
- *  设备掉线重连
- */
-@property (nonatomic, copy) HttpSendBlock DeviceReconnect;
-/**
- *  批量获取设备采集展示参数
- */
-@property (nonatomic, copy) HttpSendBlock GetDeviceDisplayData;
-/**
- *  获取房屋控制盒状态
- */
-@property (nonatomic, copy) HttpSendBlock GetHouseXFCtrlStatus;
-/**
- *  获取房屋新风控制模式
- */
-@property (nonatomic, copy) HttpSendBlock GetXFCtrlMode;
-/**
- *  设置自动控制模式平均参数
- */
-@property (nonatomic, copy) HttpSendBlock SetXFCtrlAutoModeAvgParam;
-/**
- *  获取自动控制模式平均参数
- */
-@property (nonatomic, copy) HttpSendBlock GetXFCtrlAutoModeAvgParam;
-/**
- *  设置自动控制模式采集器参数
- */
-@property (nonatomic, copy) HttpSendBlock SetXFCtrlAutoModeSensorParam;
-/**
- *  获取自动控制模式采集器参数
- */
-@property (nonatomic, copy) HttpSendBlock GetXFCtrlAutoModeSensorParam;
-/**
- *  设置房屋新风手动配置参数
- */
-@property (nonatomic, copy) HttpSendBlock SetXFCtrlManualModeParam;
-/**
- *  获取房屋新风手动配置参数
- */
-@property (nonatomic, copy) HttpSendBlock GetXFCtrlManualModeParam;
-/**
- *  设置房屋新风定时模式参数
- */
-@property (nonatomic, copy) HttpSendBlock SetXFCtrlTimerProfile;
-/**
- *  获取房屋新风定时模式参数
- */
-@property (nonatomic, copy) HttpSendBlock GetXFCtrlTimerProfile;
-/**
- *  设置房屋定时模式是否启用
- */
-@property (nonatomic, copy) HttpSendBlock SetXFCtrlTimerModeEnable;
-/**
- *  获取房屋定时模式是否启用
- */
-@property (nonatomic, copy) HttpSendBlock GetXFCtrlTimerModeEnable;
-/**
- *  获取控制盒显示数据
- */
-@property (nonatomic, copy) HttpSendBlock GetCtrlBoxDisplay;
-/**
- *  下发节点列表
- */
-@property (nonatomic, copy) HttpSendBlock DistributeNodeList;
-
+@property (nonatomic, copy) HttpSendBlock sendBlock;
+@property (nonatomic, copy) SendFailBlock sendFail;
 
 /**
  *  单例实例化TCP
@@ -130,7 +42,7 @@ typedef void(^Connect)(BOOL isConnect);
  *  @param api      发送接口
  *  @param resBlock 成功后回调
  */
-- (void)sendRequsetWithDict:(NSDictionary *)dict andApi:(NSString *)api andblock:(HttpSendBlock)resBlock;
+- (void)sendRequsetWithCommand:(NSString *)command andApi:(NSString *)api andSendBlock:(HttpSendBlock)sendBlock andFail:(SendFailBlock)fail;
 
 /**
  *  tcp连接函数
--- XZTCP.m
+++ XZTCP.m
@@ -9,7 +9,7 @@
 #import "XZTCP.h"
 #import "GCDAsyncSocket.h"
 @interface XZTCP()<GCDAsyncSocketDelegate>{
-
+    
     NSString * myHost;
     int myPort;
     BOOL isClose;
@@ -20,17 +20,17 @@
 @implementation XZTCP
 
 - (instancetype)init{
-
+    
     if (self = [super init]) {
         
         [self socketInit];
-
+        
     }
     return self;
 }
 
 - (void)socketInit{
-
+    
     dispatch_queue_t dQueue = dispatch_queue_create("tcp_dSocket_queue", NULL);
     //dispatch_queue_t sQueue = dispatch_queue_create("tcp_sSocket_queue", NULL);
     _socket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dQueue socketQueue:nil];
@@ -42,7 +42,7 @@
 }
 
 + (XZTCP *)sharedTCP{
-
+    
     static XZTCP * TCP;
     @synchronized(self) {
         
@@ -53,108 +53,22 @@
     return TCP;
 }
 
-- (void)sendRequsetWithDict:(NSMutableDictionary *)dict andApi:(NSString *)api{
-
-    if(!dict){
-    
-        dict = [[NSMutableDictionary alloc] init];
-    }
-    [dict setObject:api forKey:@"InterFace"];
-    NSData * data = [NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:nil];
-    [_socket writeData:data withTimeout:20.0f tag:200];
-}
-
-- (void)sendRequsetWithDict:(NSMutableDictionary *)dict andApi:(NSString *)api andblock:(HttpSendBlock)resBlock{
+- (void)sendRequsetWithCommand:(NSString *)command andApi:(NSString *)api andSendBlock:(HttpSendBlock)sendBlock andFail:(SendFailBlock)fail{
     
     if (![_socket isConnected]) {
         [_socket connectToHost:myHost onPort:myPort error:nil];
     }
-    if(!dict){
-        
-        dict = [[NSMutableDictionary alloc] init];
-    }
-    [dict setObject:api forKey:@"InterFace"];
-    NSData * data = [NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:nil];
     
-    if ([api isEqualToString:@"GetDeviceStatusNetwork"]) {
-        
-        [_socket writeData:data withTimeout:20.0 tag:kGetDeviceStatusNetwork];
-        [_socket readDataWithTimeout:-1 tag:kGetDeviceStatusNetwork];
-        self.GetDeviceStatusNetwork = resBlock;
-    } else if ([api isEqualToString:@"FindDeviceLocation"]){
-        [_socket writeData:data withTimeout:20.0 tag:kFindDeviceLocation];
-        [_socket readDataWithTimeout:-1 tag:kFindDeviceLocation];
-        self.FindDeviceLocation = resBlock;
-    } else if ([api isEqualToString:@"DeviceReconnect"]){
-        [_socket writeData:data withTimeout:20.0 tag:kDeviceReconnect];
-        [_socket readDataWithTimeout:-1 tag:kDeviceReconnect];
-        self.DeviceReconnect = resBlock;
-    } else if ([api isEqualToString:@"GetDeviceDisplayData"]){
-        [_socket writeData:data withTimeout:20.0 tag:kGetDeviceDisplayData];
-        [_socket readDataWithTimeout:-1 tag:kGetDeviceDisplayData];
-        self.GetDeviceDisplayData = resBlock;
-    } else if ([api isEqualToString:@"GetHouseXFCtrlStatus"]){
-        [_socket writeData:data withTimeout:20.0 tag:kGetHouseXFCtrlStatus];
-        [_socket readDataWithTimeout:-1 tag:kGetHouseXFCtrlStatus];
-        self.GetHouseXFCtrlStatus = resBlock;
-    } else if ([api isEqualToString:@"GetXFCtrlMode"]){
-        [_socket writeData:data withTimeout:20.0 tag:kGetXFCtrlMode];
-        [_socket readDataWithTimeout:-1 tag:kGetXFCtrlMode];
-        self.GetXFCtrlMode = resBlock;
-    } else if ([api isEqualToString:@"SetXFCtrlAutoModeAvgParam"]){
-        [_socket writeData:data withTimeout:20.0 tag:kSetXFCtrlAutoModeAvgParam];
-        [_socket readDataWithTimeout:-1 tag:kSetXFCtrlAutoModeAvgParam];
-        self.SetXFCtrlAutoModeAvgParam = resBlock;
-    } else if ([api isEqualToString:@"GetXFCtrlAutoModeAvgParam"]){
-        [_socket writeData:data withTimeout:20.0 tag:kGetXFCtrlAutoModeAvgParam];
-        [_socket readDataWithTimeout:-1 tag:kGetXFCtrlAutoModeAvgParam];
-        self.GetXFCtrlAutoModeAvgParam = resBlock;
-    } else if ([api isEqualToString:@"SetXFCtrlAutoModeSensorParam"]){
-        [_socket writeData:data withTimeout:20.0 tag:kSetXFCtrlAutoModeSensorParam];
-        [_socket readDataWithTimeout:-1 tag:kSetXFCtrlAutoModeSensorParam];
-        self.SetXFCtrlAutoModeSensorParam = resBlock;
-    } else if ([api isEqualToString:@"GetXFCtrlAutoModeSensorParam"]){
-        [_socket writeData:data withTimeout:20.0 tag:kGetXFCtrlAutoModeSensorParam];
-        [_socket readDataWithTimeout:-1 tag:kGetXFCtrlAutoModeSensorParam];
-        self.GetXFCtrlAutoModeSensorParam = resBlock;
-    } else if ([api isEqualToString:@"SetXFCtrlManualModeParam"]){
-        [_socket writeData:data withTimeout:20.0 tag:kSetXFCtrlManualModeParam];
-        [_socket readDataWithTimeout:-1 tag:kSetXFCtrlManualModeParam];
-        self.SetXFCtrlManualModeParam = resBlock;
-    } else if ([api isEqualToString:@"GetXFCtrlManualModeParam"]){
-        [_socket writeData:data withTimeout:20.0 tag:kGetXFCtrlManualModeParam];
-        [_socket readDataWithTimeout:-1 tag:kGetXFCtrlManualModeParam];
-        self.GetXFCtrlManualModeParam = resBlock;
-    } else if ([api isEqualToString:@"SetXFCtrlTimerProfile"]){
-        [_socket writeData:data withTimeout:20.0 tag:kSetXFCtrlTimerProfile];
-        [_socket readDataWithTimeout:-1 tag:kSetXFCtrlTimerProfile];
-        self.SetXFCtrlTimerProfile = resBlock;
-    } else if ([api isEqualToString:@"GetXFCtrlTimerProfile"]){
-        [_socket writeData:data withTimeout:20.0 tag:kGetXFCtrlTimerProfile];
-        [_socket readDataWithTimeout:-1 tag:kGetXFCtrlTimerProfile];
-        self.GetXFCtrlTimerProfile = resBlock;
-    } else if ([api isEqualToString:@"SetXFCtrlTimerModeEnable"]){
-        [_socket writeData:data withTimeout:20.0 tag:kSetXFCtrlTimerModeEnable];
-        [_socket readDataWithTimeout:-1 tag:kSetXFCtrlTimerModeEnable];
-        self.SetXFCtrlTimerModeEnable = resBlock;
-    } else if ([api isEqualToString:@"GetXFCtrlTimerModeEnable"]){
-        [_socket writeData:data withTimeout:20.0 tag:kGetXFCtrlTimerModeEnable];
-        [_socket readDataWithTimeout:-1 tag:kGetXFCtrlTimerModeEnable];
-        self.GetXFCtrlTimerModeEnable = resBlock;
-    } else if ([api isEqualToString:@"GetCtrlBoxDisplay"]){
-        [_socket writeData:data withTimeout:20.0 tag:kGetCtrlBoxDisplay];
-        [_socket readDataWithTimeout:-1 tag:kGetCtrlBoxDisplay];
-        self.GetCtrlBoxDisplay = resBlock;
-    } else if ([api isEqualToString:@"DistributeNodeList"]){
-        [_socket writeData:data withTimeout:20.0 tag:kDistributeNodeList];
-        [_socket readDataWithTimeout:-1 tag:kDistributeNodeList];
-        self.DistributeNodeList = resBlock;
-    }
+    NSData * data = [command dataUsingEncoding:NSUTF8StringEncoding];
+    [_socket writeData:data withTimeout:20.0 tag:0];
+    [_socket readDataWithTimeout:-1 tag:0];
+    self.sendBlock = sendBlock;
+    self.sendFail = fail;
     
 }
 
 - (BOOL)connectWithHost:(NSString *)hostName port:(int)port connect:(Connect)con{
-
+    
     NSError * error;
     BOOL res = [_socket connectToHost:hostName onPort:port withTimeout:-1 error:&error];
     if (![myHost isEqualToString:hostName]) {
@@ -177,7 +91,7 @@
 }
 
 - (BOOL)isConnected{
-
+    
     return [_socket isConnected];
 }
 
@@ -185,103 +99,60 @@
 
 
 - (void)socketDidCloseReadStream:(GCDAsyncSocket *)sock{
-
+    
     NSLog(@"连接断开");
 }
 
 - (void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(uint16_t)port{
-
+    
     //连接成功
-    self.tcpConnect(YES);
+    if (self.tcpConnect != nil) {
+        self.tcpConnect(YES);
+    }
+    
+    
     NSLog(@"连接成功");
 }
 - (void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(NSError *)err{
     
-<<<<<<< HEAD
-    NSLog(@"连接。。。。。。");
-=======
-    NSLog(@"连接。。");
->>>>>>> master
+    //BOOL con = self.tcpConnect(NO);
+    //连接失败
+    if (self.tcpConnect != nil) {
+        self.tcpConnect(YES);
+    }
+    NSLog(@"连接。。。。");
 }
 
 - (void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag{
-
+    
     [_socket setDelegate:nil];
     [_socket disconnect];
     [_socket setDelegate:self];
-    //NSLog(@"%@",data);
-    NSString * str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    NSData * mydata = [str dataUsingEncoding:NSUTF8StringEncoding];
-    //收到数据
-    NSDictionary * dict = [NSJSONSerialization JSONObjectWithData:mydata options:NSJSONReadingMutableContainers error:nil];
-    //NSLog(@"%@",dict);
-    NSString * apiName = dict[@"InterFace"];
-    if (apiName) {
-        
-        if ([apiName isEqualToString:@"GetDeviceStatusNetwork"]) {
-            if (_GetDeviceStatusNetwork) {
-                self.GetDeviceStatusNetwork(mydata);
-            }
-        } else if ([apiName isEqualToString:@"FindDeviceLocation"]){
-        
-            self.FindDeviceLocation(mydata);
-        } else if ([apiName isEqualToString:@"DeviceReconnect"]){
-            
-            self.DeviceReconnect(mydata);
-        } else if ([apiName isEqualToString:@"GetDeviceDisplayData"]){
-            
-            self.GetDeviceDisplayData(mydata);
-        } else if ([apiName isEqualToString:@"GetHouseXFCtrlStatus"]){
-            
-            self.GetHouseXFCtrlStatus(mydata);
-        } else if ([apiName isEqualToString:@"GetXFCtrlMode"]){
-            
-            self.GetXFCtrlMode(mydata);
-        } else if ([apiName isEqualToString:@"SetXFCtrlAutoModeAvgParam"]){
-            
-            self.SetXFCtrlAutoModeAvgParam(mydata);
-        } else if ([apiName isEqualToString:@"GetXFCtrlAutoModeAvgParam"]){
-            
-            self.GetXFCtrlAutoModeAvgParam(mydata);
-        } else if ([apiName isEqualToString:@"SetXFCtrlAutoModeSensorParam"]){
-            
-            self.SetXFCtrlAutoModeSensorParam(mydata);
-        } else if ([apiName isEqualToString:@"GetXFCtrlAutoModeSensorParam"]){
-            
-            self.GetXFCtrlAutoModeSensorParam(mydata);
-        } else if ([apiName isEqualToString:@"SetXFCtrlManualModeParam"]){
-            
-            self.SetXFCtrlManualModeParam(mydata);
-        } else if ([apiName isEqualToString:@"GetXFCtrlManualModeParam"]){
-            
-            self.GetXFCtrlManualModeParam(mydata);
-        } else if ([apiName isEqualToString:@"SetXFCtrlTimerProfile"]){
-            
-            self.SetXFCtrlTimerProfile(mydata);
-        } else if ([apiName isEqualToString:@"GetXFCtrlTimerProfile"]){
-            
-            self.GetXFCtrlTimerProfile(mydata);
-        } else if ([apiName isEqualToString:@"SetXFCtrlTimerModeEnable"]){
-            
-            self.SetXFCtrlTimerModeEnable(mydata);
-        } else if ([apiName isEqualToString:@"GetXFCtrlTimerModeEnable"]){
-            
-            self.GetXFCtrlTimerModeEnable(mydata);
-        } else if ([apiName isEqualToString:@"GetCtrlBoxDisplay"]){
-            
-            //接口弃用
-            self.GetCtrlBoxDisplay(mydata);
-        } else if ([apiName isEqualToString:@"DistributeNodeList"]){
-            
-            self.DistributeNodeList(mydata);
-        }
-    }
+    
+    self.sendBlock(data);
     
 }
 
 - (void)socket:(GCDAsyncSocket *)sock didWriteDataWithTag:(long)tag{
-
+    
     //发送数据成功
     NSLog(@"发送成功");
 }
+
+- (NSTimeInterval)socket:(GCDAsyncSocket *)sock shouldTimeoutReadWithTag:(long)tag
+                 elapsed:(NSTimeInterval)elapsed
+               bytesDone:(NSUInteger)length {
+    
+    self.sendFail();
+    return elapsed;
+}
+
+- (NSTimeInterval)socket:(GCDAsyncSocket *)sock shouldTimeoutWriteWithTag:(long)tag
+                 elapsed:(NSTimeInterval)elapsed
+               bytesDone:(NSUInteger)length {
+    
+    self.sendFail();
+    return elapsed;
+}
+
 @end
